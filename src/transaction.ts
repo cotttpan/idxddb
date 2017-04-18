5ab30b6cf3b2b98fe82a1ae642e8a7e1c8c583a9
cotttpan <cotttpan@gmail.com>
2017-04-18 23:10:47 +0900

    fix(transaction): add req.abort() and fix trx.Executor interface

--- src/transaction.ts
+++ src/transaction.ts
@@ -2,22 +2,25 @@
 export function trx<T>(scope: string | string[], mode: 'r' | 'rw', executor: trx.Executor<T>) {
     return (onResolve: Function, onReject: Function) => (db: IDBDatabase, KeyRange: typeof IDBKeyRange) => {
         const $trx = db.transaction(scope, trx.parseTrxMode(mode));
-        const i = executor(new Request<T>(KeyRange));
-
-        $trx.addEventListener('error', function () {
+        const $req = new Request<T>($trx, KeyRange);
+        const _reject = function (this: IDBTransaction) {
             onReject(this.error);
-        });
+        };
 
+        $trx.addEventListener('error', _reject);
+        $trx.addEventListener('abort', _reject);
+
+        const i = executor($req);
         (function tick(value?: any) {
             const ir = i.next(value);
-            ir.done ? onResolve(ir.value) : ir.value($trx, tick);
+            ir.done ? onResolve(ir.value) : ir.value(tick);
         }());
     };
 }
 
 export namespace trx {
     export interface Executor<T> {
-        (request: Request<T>): IterableIterator<(trx: IDBTransaction, next: Function) => IDBRequest>;
+        (request: Request<T>): IterableIterator<(next: Function) => (IDBRequest | void)>;
     }
 
     export const parseTrxMode = (mode: 'r' | 'rw') => {
@@ -26,16 +29,21 @@ export namespace trx {
 }
 
 ////////////////////// Request //////////////////////
-// TODO: req.abort()
 export class Request<T> {
     protected KeyRange: typeof IDBKeyRange;
-    constructor(KeyRange: typeof IDBKeyRange) {
+    protected trx: IDBTransaction;
+    constructor(trx: IDBTransaction, KeyRange: typeof IDBKeyRange) {
         this.KeyRange = KeyRange;
+        this.trx = trx;
+    }
+
+    abort() {
+        return () => this.trx.abort();
     }
 
     get<K extends keyof T>(store: K, key: any) {
-        return (trx: IDBTransaction, next: Function) => {
-            const req = trx.objectStore(store).get(key);
+        return (next: Function) => {
+            const req = this.trx.objectStore(store).get(key);
             req.addEventListener('success', Res.simple(next));
             return req;
         };
@@ -44,9 +52,9 @@ export class Request<T> {
     getBy<K extends keyof T>(store: K, range: Request.RangeFunction): Request.ReturnFunction;
     getBy<K extends keyof T>(store: K, index: keyof T[K], range: Request.RangeFunction): Request.ReturnFunction;
     getBy(store: string, a1: any, a2?: any): Request.ReturnFunction {
-        return (trx: IDBTransaction, next: Function) => {
+        return (next: Function) => {
             const [index, range] = typeof a1 === 'string' ? [a1, a2] : [undefined, a1];
-            const target = index ? trx.objectStore(store).index(index) : trx.objectStore(store);
+            const target = index ? this.trx.objectStore(store).index(index) : this.trx.objectStore(store);
             const req = target.openCursor(range && range(this.KeyRange));
             req.addEventListener('success', Res.matchAll(next));
             return req;
@@ -54,26 +62,26 @@ export class Request<T> {
     }
 
     getAll<K extends keyof T>(store: K) {
-        return (trx: IDBTransaction, next: Function) => {
-            const req = trx.objectStore(store).openCursor();
+        return (next: Function) => {
+            const req = this.trx.objectStore(store).openCursor();
             req.addEventListener('success', Res.matchAll(next));
             return req;
         };
     }
 
     set<K extends keyof T>(store: K, record: T[K], key?: any) {
-        return (trx: IDBTransaction, next: Function) => {
-            const req = trx.objectStore(store).put(record, key);
+        return (next: Function) => {
+            const req = this.trx.objectStore(store).put(record, key);
             req.addEventListener('success', (ev: Request.ReqEvent) => {
-                this.get(store, ev.target.result)(trx, next);
+                this.get(store, ev.target.result)(next);
             });
             return req;
         };
     }
 
     delete<K extends keyof T>(store: K, key: any) {
-        return (trx: IDBTransaction, next: Function) => {
-            const req = trx.objectStore(store).openCursor(this.KeyRange.only(key));
+        return (next: Function) => {
+            const req = this.trx.objectStore(store).openCursor(this.KeyRange.only(key));
             req.addEventListener('success', function (this: IDBRequest) {
                 const cursor: IDBCursorWithValue = this.result;
                 cursor && next(cursor.value);
@@ -85,9 +93,9 @@ export class Request<T> {
     deleteBy<K extends keyof T>(store: K, range: Request.RangeFunction): Request.ReturnFunction;
     deleteBy<K extends keyof T>(store: K, index: keyof T[K], range: Request.RangeFunction): Request.ReturnFunction;
     deleteBy(store: string, a1: any, a2?: any): Request.ReturnFunction {
-        return (trx: IDBTransaction, next: Function) => {
+        return (next: Function) => {
             const [index, range] = typeof a1 === 'string' ? [a1, a2] : [undefined, a1];
-            const target = index ? trx.objectStore(store).index(index) : trx.objectStore(store);
+            const target = index ? this.trx.objectStore(store).index(index) : this.trx.objectStore(store);
             const req = target.openCursor(range && range(this.KeyRange));
             req.addEventListener('success', Res.matchAll.withDelete(next));
             return req;
@@ -95,8 +103,8 @@ export class Request<T> {
     }
 
     clear<K extends keyof T>(store: K) {
-        return (trx: IDBTransaction, next: Function) => {
-            const req = trx.objectStore(store).openCursor();
+        return (next: Function) => {
+            const req = this.trx.objectStore(store).openCursor();
             req.addEventListener('success', Res.matchAll.withDelete(next));
             return req;
         };
@@ -106,7 +114,7 @@ export class Request<T> {
 export namespace Request {
     export type ReqEvent = Event & { target: { result: any } };
     export type RangeFunction = (keyrange: typeof IDBKeyRange) => IDBKeyRange;
-    export type ReturnFunction = (trx: IDBTransaction, next: Function) => IDBRequest;
+    export type ReturnFunction = (next: Function) => IDBRequest;
 }
 
 namespace Res {
